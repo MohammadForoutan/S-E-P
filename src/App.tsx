f6243aa9c86e022798bc0dc0fb14238bc62f9b65
mohammad <mmdfrn81@gmail.com>
2023-10-08 13:41:03 +0330

    chore: add mantine

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import "@mantine/core/styles.css";
-import { MantineProvider } from "@mantine/core";
+import { MantineProvider, Switch } from "@mantine/core";
 import { theme } from "./theme";
+import { Route } from "react-router-dom";
+import { Auth } from "./pages/auth/auth";
 
 export default function App() {
-  return <MantineProvider theme={theme}>App</MantineProvider>;
+  return <MantineProvider theme={theme}>
+    <Switch>
+      <Route path="/login">
+        <Auth />
+      </Route>
+      <Route path="*">
+        <Auth />
+      </Route>
+    </Switch>
+  </MantineProvider>;
 }
--- src/theme.ts
+++ src/theme.ts
@@ -1,5 +1,22 @@
-import { createTheme } from "@mantine/core";
+import { MantineColorsTuple, createTheme } from "@mantine/core";
+
+const greenColor: MantineColorsTuple = [
+  '#e5feee',
+  '#d2f9e0',
+  '#a8f1c0',
+  '#7aea9f',
+  '#53e383',
+  '#3bdf70',
+  '#2bdd66',
+  '#1ac455',
+  '#0caf49',
+  '#00963c'
+];
 
 export const theme = createTheme({
   /* Put your mantine theme override here */
+  colors: {
+    greenColor
+  }
+
 });
