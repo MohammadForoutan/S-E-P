f383513dfe7d0b234c45b8c60eebe4310a6b080c
mohammad foroutan <mmdfrn81@gmail.com>
2023-11-22 13:39:55 +0330

    add fa/en to discussion and fix navbar height

--- src/components/Discussion/Discussion.tsx
+++ src/components/Discussion/Discussion.tsx
@@ -1,22 +1,63 @@
-import { Group, Paper } from "@mantine/core";
+import { Flex, Grid, Group, Paper } from "@mantine/core";
 import { IconUserCircle } from "@tabler/icons-react";
+import { useLangStore } from "../../stores/langStore";
+import { LANGS } from "../../i18n/locales/type";
 
 type Props = {
   id: number;
 };
 export default function Discussion({ id }: Props) {
+  const langStore = useLangStore();
   return (
-    <div>
-      <Paper withBorder px={13} mb={12}>
+    <Flex direction={"column"}>
+      <Paper
+        style={{ justifyContent: "space-between" }}
+        display={"flex"}
+        withBorder
+        px={13}
+        mb={12}
+      >
         <Group>
           <IconUserCircle size={37} color="blue" />
           <p>محمدرضا فروتن</p>
           <p>{id}</p>
         </Group>
+        <Group>
+          <p>
+            {langStore.lang === LANGS.en_US
+              ? new Date().toLocaleDateString("us-en", {
+                  year: "numeric",
+                  month: "long",
+                  day: "numeric",
+                  hour: "numeric",
+                  minute: "2-digit",
+                })
+              : new Date().toLocaleDateString("fa-ir", {
+                  year: "numeric",
+                  month: "long",
+                  day: "numeric",
+                  hour: "numeric",
+                  minute: "2-digit",
+                })}
+          </p>
+        </Group>
       </Paper>
 
-      <main>tickets array here - like chats and messages</main>
-      <div>ChatBox Component here</div>
-    </div>
+      <div>
+        <Paper bg={"green"} h={900}></Paper>
+        <main>tickets array here - like chats and messages</main>
+      </div>
+
+      <Paper
+        withBorder
+        mt={"auto"}
+        bg={"gray"}
+        p={12}
+        pos={"sticky"}
+        w={"100%"}
+      >
+        <div>ChatBox Component here</div>
+      </Paper>
+    </Flex>
   );
 }
