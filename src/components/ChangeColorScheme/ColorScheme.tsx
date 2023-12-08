d08f93e062c4601e8225ad07733386a1572492fa
mohammad foroutan <mmdfrn81@gmail.com>
2023-12-08 15:31:30 +0330

    fix scheme mode

--- src/components/ChangeColorScheme/ColorScheme.tsx
+++ src/components/ChangeColorScheme/ColorScheme.tsx
@@ -1,11 +1,11 @@
 import { Select } from "@mantine/core";
-import { SCHEME, useSchemeStore } from "../../stores/themeStore";
+import { useSchemeStore } from "../../stores/themeStore";
 import { useTranslation } from "react-i18next";
 
 export function ChangeScheme() {
   const { t } = useTranslation("menu");
 
-  const updateScheme = (scheme: SCHEME) => {
+  const updateScheme = (scheme: any) => {
     schemeStore.updateScheme(scheme);
   };
 
@@ -20,7 +20,7 @@ export function ChangeScheme() {
           { label: t("dark"), value: "dark" },
           { label: t("light"), value: "light" },
         ]}
-        onChange={() => updateScheme}
+        onChange={updateScheme}
         allowDeselect={false}
       />
     </div>
